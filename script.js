280cac456f0144bc0100c9dc92b6a379e23c6a1f
aathilasathik0525 <aathilasathik0525@gmail.com>
2026-05-02 18:16:26 +0530

    Added secondProject

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-js
+let display = document.getElementById("display");
+
+function append(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let result = eval(display.value);
+
+        if (!isFinite(result)) {
+            display.value = "Error";
+        } else {
+            display.value = result;
+        }
+    } catch {
+        display.value = "Error";
+    }
+}
